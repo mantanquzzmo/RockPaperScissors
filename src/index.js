7893a0502e3cbaf95a3c4367913642d484a5e16d
mantanquzzmo <quzzmo@hotmail.com>
2019-11-30 14:40:18 +0100

    Completes setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const StartingPage = () => {
+    return <h2>RockPaperScissors</h2>
+};
+
+ReactDOM.render(<StartingPage />, document.getElementById("app"))
